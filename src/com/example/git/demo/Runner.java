31fba6766a0182cc4f9ad4dfaace2f343205c2a6
jbrayo <jbrayo@up.edu.ph>
2014-05-29 15:30:42 +0800

    added another sysout

--- src/com/example/git/demo/Runner.java
+++ src/com/example/git/demo/Runner.java
@@ -5,6 +5,7 @@ public class Runner {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git demo");
 		System.out.println("second println");
+		System.out.println("yey new branch");
 	}
 
 }
