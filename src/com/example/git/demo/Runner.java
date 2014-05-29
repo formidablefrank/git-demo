24a4347755655d43334e1b4fae6d2f699d9bbaf4
jbrayo <jbrayo@up.edu.ph>
2014-05-29 15:42:39 +0800

    commit

--- src/com/example/git/demo/Runner.java
+++ src/com/example/git/demo/Runner.java
@@ -4,8 +4,11 @@ public class Runner {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git demo");
+<<<<<<< HEAD
 		System.out.println("second println");
 		System.out.println("yey new branch");
+=======
+>>>>>>> parent of 3bc9ba5... added a line
 	}
 
 }
