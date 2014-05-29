fe9ba77cf55ddd05524254d321eea2c5083ed06f
jbrayo <jbrayo@up.edu.ph>
2014-05-29 16:04:04 +0800

    Revert "added a line"
    This reverts commit 3bc9ba52185f2b5eecc748995249405f63e34009.
    
    Conflicts:
    	src/com/example/git/demo/Runner.java

--- src/com/example/git/demo/Runner.java
+++ src/com/example/git/demo/Runner.java
@@ -4,11 +4,11 @@ public class Runner {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git demo");
-<<<<<<< HEAD
 		System.out.println("second println");
 		System.out.println("yey new branch");
-=======
->>>>>>> parent of 3bc9ba5... added a line
+		System.out.println("yey new branch");
+		System.out.println("yey new branch");
+		System.out.println("yey new branch");
 	}
 
 }
